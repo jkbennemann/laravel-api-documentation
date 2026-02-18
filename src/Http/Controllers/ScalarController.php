cc74ab91dc3ba8a04621f68d82f081dd09fde2f4
jakob.bennemann <jakob.bennemann@team.blue>
2026-02-18 22:27:51 +0100

    feat: add proxy configuration option

--- src/Http/Controllers/ScalarController.php
+++ src/Http/Controllers/ScalarController.php
@@ -15,25 +15,31 @@ class ScalarController
         $proxyUrl = config('api-documentation.ui.scalar.proxy_url', 'https://proxy.scalar.com');
 
         $filename = config('api-documentation.ui.storage.filename', null);
-        $oldFile[] = [
+        $oldFile = [
             'name' => $filename,
             'url' => $filename ? asset($filename) : null,
-            'proxyUrl' => $proxyUrl ?: null,
         ];
+        if ($proxyUrl) {
+            $oldFile['proxyUrl'] = $proxyUrl;
+        }
+        $oldFiles[] = $oldFile;
 
         $files = [];
         foreach (config('api-documentation.ui.storage.files', []) as $key => $file) {
             if ($this->check($key)) {
-                $files[] = [
+                $entry = [
                     'title' => $file['filename'],
                     'url' => asset($file['filename']),
-                    'proxyUrl' => $proxyUrl ?: null,
                 ];
+                if ($proxyUrl) {
+                    $entry['proxyUrl'] = $proxyUrl;
+                }
+                $files[] = $entry;
             }
         }
 
         return view('api-documentation::scalar.index', [
-            'files' => ! empty($files) ? $files : $oldFile,
+            'files' => ! empty($files) ? $files : $oldFiles,
         ]);
     }
 }
